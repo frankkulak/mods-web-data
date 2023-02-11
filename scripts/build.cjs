da0cea09e2879abb07926b12aaaacd0cc3ca0797
Frank Kulak <fs.kulak@gmail.com>
2023-02-11 10:36:02 -0500

    Replaced minified folder with build.

---
+++ scripts/build.cjs
@@ -0,0 +1,44 @@
+const fs = require("fs");
+const path = require("path");
+const glob = require("glob");
+
+const DATA_DIR = path.resolve(__dirname, "../data");
+const BUILD_DIR = path.resolve(__dirname, "../build");
+
+//#region Helpers
+
+/**
+ * Figures out where the built file should be written to and ensures that all
+ * necessary folders exist.
+ *
+ * @param {string} sourcePath File to source file
+ * @returns {string}
+ */
+function getDestinationPath(sourcePath) {
+  const rel = sourcePath.replace(DATA_DIR, "");
+  const dest = path.join(BUILD_DIR, rel);
+  const dir = path.dirname(dest);
+  if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+  return dest;
+}
+
+/**
+ * Reads the JSON at the given filepath, removes the schema, and writes it to
+ * the build directory without any whitespace.
+ *
+ * @param {string} filepath Path to source file to minify
+ */
+function minifyJson(filepath) {
+  const dest = getDestinationPath(filepath);
+  const json = JSON.parse(fs.readFileSync(filepath).toString());
+  delete json.$schema;
+  fs.writeFileSync(dest, JSON.stringify(json));
+}
+
+//#endregion
+
+//#region Build
+
+// TODO:
+
+//#endregion
